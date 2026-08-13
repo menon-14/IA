aa7c9b1af365f1f6be8982a80ba60aad7887f531
menon-14 <mariana.menon@escola.pr.gov.br>
2026-08-13 08:32:40 -0300

    juytrdcv

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaPerguntas = document.querySelector(".caixa-alternativas");
+const caixaPerguntas = document.querySelector(".caixa-alternativas");
+const textoResultado = document.querySelector(".texto-resultado");
+
+constconst perguntas = [
+    {
+    enunciado: "Pergunta 1", 
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+ },   
+    {
+    enunciado: "Pergunta 2", 
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+ }  
+  
+]
